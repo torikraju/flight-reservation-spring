884ecf24100a322236c3edd675380ad4cc7e9d5f
Torikul Alam <torikul.alam@shebatech.com.bd>
2020-02-23 13:09:50 +0600

    added user entity

---
+++ src/main/java/com/flightreservation/entity/User.java
@@ -0,0 +1,26 @@
+package com.flightreservation.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "USER")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "FIRST_NAME")
+    private String firstName;
+
+    @Column(name = "LAST_NAME")
+    private String lastName;
+
+    @Column(name = "EMAIL", unique = true)
+    private String email;
+
+    @Column(name = "PASSWORD")
+    private String password;
+}
